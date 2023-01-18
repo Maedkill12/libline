5efd5bdfc96f6022a0fac4277524380163154495
maed <maed012014@gmail.com>
2023-01-18 10:42:06 -0600

    Added register feature

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,65 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import IconButton from "../components/IconButton";
 import Input from "../components/Input";
+import { URL_API } from "../constants";
 
 const Register = () => {
   const [username, setUsernmae] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPass, setConfirmPass] = useState("");
-  const submitHandler = (e) => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
     e.preventDefault();
+    setError(null);
+
+    if (!username || !email || !password || !confirmPass) {
+      setError("All fields are required");
+      return;
+    }
+
+    if (username.length < 3 || username.length > 20) {
+      setError("Username length must be between 3 and 20 characters");
+      return;
+    }
+
+    if (password.length < 3) {
+      setError("Password must be at least 8 characters");
+      return;
+    }
+
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      setError("Invalid email");
+      return;
+    }
+
+    if (password !== confirmPass) {
+      setError("Password doesn't match");
+      return;
+    }
+
+    try {
+      const response = await axios.post(`${URL_API}/auth/register`, {
+        username,
+        email,
+        password,
+      });
+      const { data } = response;
+      if (data.success) {
+        navigate("/login");
+      }
+    } catch (error) {
+      const msg = error.response.data.err;
+      setError(msg);
+    }
   };
 
   return (
@@ -57,7 +108,7 @@ const Register = () => {
             inputOptions={{
               id: "confirmPassword",
               placeholder: "Confirm your password",
-              type: "confirmPassword",
+              type: "password",
               onChange: (e) => setConfirmPass(e.target.value),
               value: confirmPass,
             }}
@@ -69,6 +120,11 @@ const Register = () => {
             Sign Up
           </IconButton>
         </form>
+        {error && (
+          <div className="mt-4 text-center text-red-700 font-bold">
+            Error: {error}
+          </div>
+        )}
       </div>
     </div>
   );
