85db2805508f5affcf97cfd3d3614203f9e61a6b
maed <maed012014@gmail.com>
2023-01-17 18:41:03 -0600

    Added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import IconButton from "../components/IconButton";
+import Input from "../components/Input";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsernmae] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <div className="bg-white w-[400px] py-4 px-4 rounded-2xl shadow-lg shadow-slate-500/40">
+        <h1 className="text-center text-2xl font-bold font-sans text-slate-800 mb-4">
+          Login
+        </h1>
+        <form onSubmit={submitHandler}>
+          <Input
+            inputOptions={{
+              id: "username",
+              placeholder: "Type your username",
+              onChange: (e) => setUsernmae(e.target.value),
+              value: username,
+            }}
+            textLabel={"Username"}
+            required={true}
+            style="mb-4"
+          />
+          <Input
+            inputOptions={{
+              id: "password",
+              placeholder: "Type your password",
+              type: "password",
+              onChange: (e) => setPassword(e.target.value),
+              value: password,
+            }}
+            textLabel="Password"
+            required={true}
+            style="mb-4"
+          />
+          <IconButton style={"justify-center w-full"} type="submit">
+            Login
+          </IconButton>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
