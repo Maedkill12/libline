f67a4c4f02e00133822ace94384ed0bc6df7fbf8
maed <maed012014@gmail.com>
2023-01-19 12:52:15 -0600

    Changed logo shape

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,10 +4,12 @@ import icon from "../assets/icon.png";
 const Logo = () => {
   return (
     <div className="flex flex-row items-center gap-3 cursor-pointer py-4 px-2">
-      <img
-        className="w-[64px] h-[64px] object-cover rounded-lg shadow-md shadow-slate-900"
-        src={icon}
-      />
+      <div className="w-[64px] h-[64px] rounded-full overflow-hidden ">
+        <img
+          className="w-[80px] h-[70px] object-cover rounded-lg shadow-md shadow-slate-900"
+          src={icon}
+        />
+      </div>
       <h1 className="text-lg font-serif font-bold text-slate-100 ">LIBLINE</h1>
     </div>
   );
